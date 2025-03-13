9d8323aeda2b834f255584b96a907ab5884fb8cf
aatifahmad123 <b22ai002@iitj.ac.in>
2025-03-14 02:26:16 +0530

    Vraiables and DataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 123
+
+let accountEmail = "ahmadaatif6@gmail.com"
+
+var accountPassword = "Typescript-Go"
+
+// Prefer not to use var due to block scoping and functional scoping
+
+/*
+
+In JavaScript, let and var both declare variables, but let is block-scoped (accessible within the block where it's declared), while var is function-scoped (accessible throughout the function where it's declared).
+ 
+*/
+
+accountCity = "Jaipur"
+
+accountEmail = "b22ai002@iitj.ac.in"
+accountPassword = "React-Redux"
+accountCity = "Bengaluru"
+
+let accountState;
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // to use the newer version of javascript
+
+// alert (3+3) we are using node not the browser
+
+console.log("Newer version")
+
+/*
+
+Data Types:
+
+Number
+BigInt
+String - ""
+Boolean - true/false
+null - standalone value
+undefined
+symbol - uniqueness
+
+*/
+
+// object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
