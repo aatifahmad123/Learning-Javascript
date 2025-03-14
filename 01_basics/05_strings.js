f42917736999ae677aea4fe7036327629be2c4c7
aatifahmad123 <b22ai002@iitj.ac.in>
2025-03-14 19:38:21 +0530

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,13 @@
+let name = "Virat"
+
+console.log(`The players's name is ${name}`); // string interpolation
+
+const player = new String("Steve");
+
+console.log(player.charAt(2))
+console.log(player.toUpperCase())
+console.log(player.toLowerCase())
+console.log(player.replace("ve","ph"))
+console.log(player.split(""))
+console.log(player.substr(1,3))
+console.log(player.slice(-4,4))
